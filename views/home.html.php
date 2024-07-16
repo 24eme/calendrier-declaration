6310e7a1bfcef407c9e1dd31c548cda72b74cd7b
JB LM <lemetayer.jb@gmail.com>
2024-07-16 17:58:51 +0200

    affichage du calendrier a mois -1 sur 1 année glissante

--- views/home.html.php
+++ views/home.html.php
@@ -4,12 +4,18 @@
 
   <div class="container-fluid text-center">
     <div class="row sticky-top">
-      <?php for($m = 1; $m<=12; $m++): ?>
-      <div class="col-1 bg-warning border-start border-end border-white border-2">
-        <?php $month = date('M', mktime(0, 0, 0, $m, 1, date('Y'))); ?>
-        <?php echo ucfirst($month); ?>
+      <?php
+        $date = new DateTime();
+        $date->modify('-1 month');
+        for($i = 0; $i < 12; $i++):
+      ?>
+      <div class="col-1 bg-warning border-start border-end border-white border-1">
+        <?php echo $date->format('M'); ?>
       </div>
-      <?php endfor; ?>
+      <?php
+        $date->modify('+1 month');
+        endfor;
+      ?>
     </div>
 
     <?php foreach ($evenements as $titre => $evts): ?>
@@ -17,13 +23,18 @@
       <div class="text-start w-100">
         <?php echo $titre ?>
       </div>
-
-      <?php for ($mois = 1; $mois <= 12; $mois++): ?>
-        <div class="col-1 px-0 pb-2 border-start border-end border-white border-2">
-          <?php echo \Views\MonthTimeline::render($mois, $evts); ?>
-        </div>
-      <?php endfor; ?>
-
+      <?php
+        $date = new DateTime();
+        $date->modify('-1 month');
+        for($i = 0; $i < 12; $i++):
+      ?>
+      <div class="col-1 px-0 pb-2 border-start border-end border-white border-2">
+        <?php echo \Views\MonthTimeline::render($date->format('n'), $evts); ?>
+      </div>
+      <?php
+        $date->modify('+1 month');
+        endfor;
+      ?>
     </div>
     <?php endforeach; ?>
   </div>
