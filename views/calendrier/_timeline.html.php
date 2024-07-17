ccf4d0b5bf1628b33ea7d718fd653ed0d04ac6af
Vincent LAURENT <vince.laurent@gmail.com>
2024-07-17 20:08:00 +0200

    Tous en variable et dans le css

--- views/calendrier/_timeline.html.php
+++ views/calendrier/_timeline.html.php
@@ -1,8 +1,6 @@
-<div class="ligne">
   <?php for($j = 1; $j <= $nbDays; $j++): ?>
     <span title="<?php echo $current->format("d/m/Y") ?>"
         class="<?php echo \Views\MonthTimeline::getTimelineClass($events, $current, $today) ?>"></span>
 
     <?php $current->modify('+1 day'); ?>
   <?php endfor; ?>
-</div>
--- views/home.html.php
+++ views/home.html.php
@@ -1,38 +1,37 @@
-<div id="calendrierVue" style="width: max-content; ">
-    <div class="cal-ligne cal-ligne-head">
-      <div class="cal-titre">
+  <div id="calendar">
+      <div class="cal-titre cal-titre-header"></div>
+      <div class="cal-ligne cal-ligne-head shadow-sm">
+        <?php
+          $date = new DateTime(date('Y-m-d'));
+          $date->modify('first day of previous month');
+          for($i = 0; $i < 16; $i++):
+        ?>
+        <div class="cal-month" data-nbdays="<?php echo ($date->format('t')); ?>">
+          <?php echo $date->format('M Y'); ?>
+        </div>
+        <?php
+          $date->modify('next month');
+          endfor;
+        ?>
       </div>
-      <?php
-        $date = new DateTime(date('Y-m-d'));
-        $date->modify('first day of previous month');
-        for($i = 0; $i < 16; $i++):
-      ?>
-      <div class="cal-month" style="width: <?php echo ($date->format('t')+1)*3; ?>px;">
-        <?php echo $date->format('M Y'); ?>
+      <?php foreach ($evenements as $titre => $evts): ?>
+      <div class="cal-ligne" style="clear: both;">
+        <div class="cal-titre" title="<?php echo $titre ?>">
+          <?php echo $titre ?>
+        </div>
+        <?php
+          $date = new DateTime();
+          $date->modify('first day of previous month');
+          for($i = 0; $i < 16; $i++):
+        ?>
+        <div class="cal-month">
+          <?php echo \Views\MonthTimeline::render($date, $evts); ?>
+        </div>
+        <?php
+          $date->modify('next month');
+          endfor;
+        ?>
       </div>
-      <?php
-        $date->modify('next month');
-        endfor;
-      ?>
-    </div>
-
-    <?php foreach ($evenements as $titre => $evts): ?>
-    <div class="cal-ligne">
-      <div class="cal-titre">
-        <?php echo $titre ?>
-      </div>
-      <?php
-        $date = new DateTime();
-        $date->modify('first day of previous month');
-        for($i = 0; $i < 16; $i++):
-      ?>
-      <div class="cal-month">
-        <?php echo \Views\MonthTimeline::render($date, $evts); ?>
-      </div>
-      <?php
-        $date->modify('next month');
-        endfor;
-      ?>
-    </div>
-    <?php endforeach; ?>
+      <?php endforeach; ?>
+  </div>
 </div>
