720b547b1047f1f5990c96a0e1cac0e05790a3ae
JB LM <lemetayer.jb@gmail.com>
2024-08-05 18:39:20 +0200

    page evenement en mode modal

--- views/calendrier/evenement.html.php
+++ views/calendrier/evenement.html.php
@@ -1,63 +1,73 @@
-<div class="container">
-  <div class="row sticky-top bg-white pt-2">
-    <div class="col">
-      <h2 class="h2"><?php echo $event->nom ?>
-        <small><a href="<?php echo Base::instance()->alias('eventedit', ['evenement' => $event->id]) ?>"><i class="bi bi-pencil-square"></i></a></small>
-        <?php if ($event->liendeclaration): ?>
-        <a href="<?php echo $event->liendeclaration ?>" class="btn btn-danger float-end">
-          Accéder à la déclaration <i class="d-inline-flex bi bi-box-arrow-up-right"></i>
-        </a>
-        <?php endif ?>
-        <a href="/evenement/export/<?php echo $event->id ?>" class="btn btn-primary float-end mx-2" title="Exporter la déclaration dans mon calendrier personnel"><i class="bi bi-calendar-plus"></i></a>
-      </h2>
-      <p class="tags-header">
-        <span><i class="bi bi-bookmark me-1"></i> <?php echo $event->type_id->nom ?></span>
-        <span><i class="bi bi-person-rolodex me-1"></i> <?php echo implode(', ', $event->familles->getAll('nom')) ?></span>
-        <?php if($event->recurrence): ?>
-        <span><i class="bi bi-calendar-range me-1"></i> Déclaration <?php echo $event->recurrence ?>le</span>
-        <?php endif; ?>
-      </p>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col-7">
-      <h3 class="h3">Information</h3>
-      <p class="mt-4 mb-0">
-        <?php echo $event->description ?>
-      </p>
-    </div>
-
-    <div class="col-5">
-      <?php foreach ($event->organismes as $organisme): ?>
-        <div class="organisme-card p-2">
-          <div class="pb-2">
-            <img src="/images/logos/organismes/<?php echo $organisme->logo ?>" class="img-fluid" height="20px">
-            <strong><?php echo $organisme->nom ?></strong>
-          </div>
-          <div>
-            <?php echo nl2br($organisme->adresse . PHP_EOL . $organisme->code_postal . " " . $organisme->ville) ?>
-          </div>
-          <div>
-            <i class="bi bi-telephone-fill"></i> <?php echo $organisme->telephone ?> <br/>
-            <i class="bi bi-envelope"></i> <?php echo $organisme->email ?> <br/>
-            <i class="bi bi-globe"></i> <a href="<?php echo $organisme->site ?>"><?php echo $organisme->site ?></a>
-          </div>
+<div class="modal-xl modal show d-block">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content p-3">
+      <div class="row sticky-top bg-white pt-2">
+        <div class="col">
+          <h2 class="h2"><?php echo $event->nom ?>
+            <small><a href="<?php echo Base::instance()->alias('eventedit', ['evenement' => $event->id]) ?>"><i class="bi bi-pencil-square"></i></a></small>
+            <a href="/" class="ms-3 float-end">
+              <i class="bi bi-x-circle"></i>
+            </a>
+            <?php if ($event->liendeclaration): ?>
+            <a href="<?php echo $event->liendeclaration ?>" class="btn btn-danger float-end">
+              Accéder à la déclaration <i class="d-inline-flex bi bi-box-arrow-up-right"></i>
+            </a>
+            <?php endif ?>
+            <a href="/evenement/export/<?php echo $event->id ?>" class="btn btn-primary float-end mx-2" title="Exporter la déclaration dans mon calendrier personnel"><i class="bi bi-calendar-plus"></i></a>
+          </h2>
+          <p class="tags-header">
+            <span><i class="bi bi-bookmark me-1"></i> <?php echo $event->type_id->nom ?></span>
+            <span><i class="bi bi-person-rolodex me-1"></i> <?php echo implode(', ', $event->familles->getAll('nom')) ?></span>
+            <?php if($event->recurrence): ?>
+            <span><i class="bi bi-calendar-range me-1"></i> Déclaration <?php echo $event->recurrence ?>le</span>
+            <?php endif; ?>
+          </p>
         </div>
-      <?php endforeach ?>
+      </div>
+
+      <div class="row">
+        <div class="col-7">
+          <h3 class="h3">Information</h3>
+          <p class="mt-4 mb-0">
+            <?php echo $event->description ?>
+          </p>
+        </div>
+
+        <div class="col-5">
+          <?php foreach ($event->organismes as $organisme): ?>
+            <div class="organisme-card p-2">
+              <div class="pb-2">
+                <img src="/images/logos/organismes/<?php echo $organisme->logo ?>" class="img-fluid" height="20px">
+                <strong><?php echo $organisme->nom ?></strong>
+              </div>
+              <div>
+                <?php echo nl2br($organisme->adresse . PHP_EOL . $organisme->code_postal . " " . $organisme->ville) ?>
+              </div>
+              <div>
+                <i class="bi bi-telephone-fill"></i> <?php echo $organisme->telephone ?> <br/>
+                <i class="bi bi-envelope"></i> <?php echo $organisme->email ?> <br/>
+                <i class="bi bi-globe"></i> <a href="<?php echo $organisme->site ?>"><?php echo $organisme->site ?></a>
+              </div>
+            </div>
+          <?php endforeach ?>
+        </div>
+      </div>
+
+      <hr class="w-50 mx-auto"/>
+
+      <div class="row">
+        <p>
+            <b class="text-decoration-underline me-3">Mots-clés :</b>
+            <?php foreach ($event->tags as $tag): ?>
+              <a href="/?<?php echo http_build_query(['filters' => ['tags' => [$tag->id => 'on']]]) ?>" class="btn btn-sm btn-outline-primary">
+                <?php echo $tag->nom ?>
+              </a>
+            <?php endforeach ?>
+            <a href="/" class="btn btn-secondary float-end">Fermer</a>
+        </p>
+      </div>
     </div>
-  </div>
-
-  <hr class="w-50 mx-auto"/>
-
-  <div class="row">
-    <p>
-        <b class="text-decoration-underline me-3">Mots-clés :</b>
-        <?php foreach ($event->tags as $tag): ?>
-          <a href="/?<?php echo http_build_query(['filters' => ['tags' => [$tag->id => 'on']]]) ?>" class="btn btn-sm btn-outline-primary">
-            <?php echo $tag->nom ?>
-          </a>
-        <?php endforeach ?>
-    </p>
   </div>
 </div>
+
+<div class="modal-backdrop" style="opacity: 90%;"></div>
