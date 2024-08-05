2df0cf70e0afc1abeb4e83d6f13d9488762a7aa7
JB LM <lemetayer.jb@gmail.com>
2024-08-05 18:41:14 +0200

    reindentation

--- views/home.html.php
+++ views/home.html.php
@@ -1,40 +1,40 @@
-  <div id="calendar">
-    <div class="cal-header">
-      <div class="cal-titre cal-titre-header"></div>
-      <div class="cal-ligne cal-ligne-head shadow-sm">
-        <?php
-          $date = DateTime::createFromImmutable($today);
-          $date->modify('first day of previous month');
-          for($i = 0; $i < Models\Evenement::$displayMonths; $i++):
-        ?>
-        <div class="cal-month" data-nbdays="<?php echo ($date->format('t')); ?>">
-          <?php echo $date->format('M Y'); ?>
-        </div>
-        <?php
-          $date->modify('next month');
-          endfor;
-        ?>
+<div id="calendar">
+  <div class="cal-header">
+    <div class="cal-titre cal-titre-header"></div>
+    <div class="cal-ligne cal-ligne-head shadow-sm">
+      <?php
+        $date = DateTime::createFromImmutable($today);
+        $date->modify('first day of previous month');
+        for($i = 0; $i < Models\Evenement::$displayMonths; $i++):
+      ?>
+      <div class="cal-month" data-nbdays="<?php echo ($date->format('t')); ?>">
+        <?php echo $date->format('M Y'); ?>
       </div>
-    </div>
-    <div class="cal-events">
-      <?php foreach ($evenements as $nom => $evts): ?>
-      <div class="cal-ligne">
-        <div class="cal-titre" title="<?php echo $nom ?>">
-          <a href="<?php echo Base::instance()->alias('event', ['evenement' => current($evts)['id']]) ?>"><?php echo $nom ?></a>
-        </div>
-        <?php
-          $date = DateTime::createFromImmutable($today);
-          $date->modify('first day of previous month');
-          for($i = 0; $i < Models\Evenement::$displayMonths; $i++):
-        ?>
-        <div class="cal-month">
-          <?php echo \Views\MonthTimeline::render($date, $today, $evts); ?>
-        </div>
-        <?php
-          $date->modify('next month');
-          endfor;
-        ?>
-      </div>
-      <?php endforeach; ?>
+      <?php
+        $date->modify('next month');
+        endfor;
+      ?>
     </div>
   </div>
+  <div class="cal-events">
+    <?php foreach ($evenements as $nom => $evts): ?>
+    <div class="cal-ligne">
+      <div class="cal-titre" title="<?php echo $nom ?>">
+        <a href="<?php echo Base::instance()->alias('event', ['evenement' => current($evts)['id']]) ?>"><?php echo $nom ?></a>
+      </div>
+      <?php
+        $date = DateTime::createFromImmutable($today);
+        $date->modify('first day of previous month');
+        for($i = 0; $i < Models\Evenement::$displayMonths; $i++):
+      ?>
+      <div class="cal-month">
+        <?php echo \Views\MonthTimeline::render($date, $today, $evts); ?>
+      </div>
+      <?php
+        $date->modify('next month');
+        endfor;
+      ?>
+    </div>
+    <?php endforeach; ?>
+  </div>
+</div>
