73cb7611d501cfdf844f8f4bd05c1248352075ab
tale-fau <tale-fau@24eme.fr>
2024-08-28 17:56:48 +0200

    Theme rhone

---
+++ public/themes/rhone/css.php
@@ -0,0 +1,33 @@
+<style>
+
+    :root {
+        --bs-primary: #80002d;
+    }
+
+    .btn-outline-primary {
+    --bs-btn-color: #80002d;
+    --bs-btn-border-color: #80002d;
+    --bs-btn-hover-bg: #80002d;
+    --bs-btn-hover-border-color: #80002d;
+    --bs-btn-active-bg: #80002d;
+    --bs-btn-active-border-color: #80002d;
+    --bs-btn-disabled-color: #80002d;
+    --bs-btn-disabled-border-color: #80002d;
+    }
+
+    .btn-primary {
+      --bs-btn-bg: #80002d;
+      --bs-btn-border-color: #80002d;
+      --bs-btn-disabled-bg: #80002d;
+      --bs-btn-disabled-border-color: #80002d;
+    }
+
+    .form-check-input:checked {
+      background-color: #80002d;
+      border-color: #80002d;
+    }
+
+    a {
+        color: #80002d;
+    }
+</style>
---
+++ public/themes/rhone/header.php
@@ -0,0 +1,1 @@
+<img class="my-4" src="https://declaration.syndicat-cotesdurhone.com/images/logo_odgrhone.png" height="90px" alt="logo"/>
