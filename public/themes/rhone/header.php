e46c0e2562f9cafd83e0995e32678216e93e46f5
JB LM <lemetayer.jb@gmail.com>
2024-09-13 18:50:18 +0200

    calage logo

--- public/themes/rhone/header.php
+++ public/themes/rhone/header.php
@@ -1,1 +1,1 @@
-<img class="my-4" src="https://declaration.syndicat-cotesdurhone.com/images/logo_odgrhone.png" height="90px" alt="logo"/>
+<img class="my-3 ms-5 ps-3" src="https://declaration.syndicat-cotesdurhone.com/images/logo_odgrhone.png" height="90px" alt="logo"/>
