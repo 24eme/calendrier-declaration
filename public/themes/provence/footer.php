01800b36e8555e64ac9a4bf8d6485aa8369b3408
JB LM <lemetayer.jb@gmail.com>
2024-09-16 12:32:09 +0200

    mentions legales pour provence

--- public/themes/provence/footer.php
+++ public/themes/provence/footer.php
@@ -10,7 +10,7 @@
       <a class="nav-link" id="nav-link-contact" href="https://www.vinsdeprovence.com/contact">Contact</a>
     </li>
     <li class="nav-item d-inline-block mx-2" id="nav-item-mentions-legales" role="menuitem">
-      <a class="nav-link" id="nav-link-mentions-legales" href="https://calendrier-vitivini.vinsdeprovence.com/mentions-legales">Mentions légales</a>
+      <a class="nav-link" id="nav-link-mentions-legales" href="/pages/mentions-legales">Mentions légales</a>
     </li>
     <li class="nav-item d-inline-block mx-2" id="nav-item-admin" role="menuitem">
       <a class="nav-link" id="nav-link-admin" href="<?php echo Base::instance()->alias('login') ?>"><i class="fas fa-user-lock"></i> Administration</a>
