8d0f9d41408a523ab84da4cad87eb5ea5f0b6181
Vincent LAURENT <vince.laurent@gmail.com>
2024-09-20 15:09:51 +0200

    Css plus générique

--- public/themes/rhone/css.php
+++ public/themes/rhone/css.php
@@ -1,65 +1,12 @@
 <style>
-
     :root {
         --primary-color: #80002d;
         --bs-primary: var(--primary-color);
-    }
-
-    .btn-outline-primary {
-    --bs-btn-color: var(--primary-color);
-    --bs-btn-border-color: var(--primary-color);
-    --bs-btn-hover-bg: var(--primary-color);
-    --bs-btn-hover-border-color: var(--primary-color);
-    --bs-btn-active-bg: var(--primary-color);
-    --bs-btn-active-border-color: var(--primary-color);
-    --bs-btn-disabled-color: var(--primary-color);
-    --bs-btn-disabled-border-color: var(--primary-color);
-    }
-
-    .btn-primary {
-      --bs-btn-bg: var(--primary-color);
-      --bs-btn-border-color: var(--primary-color);
-      --bs-btn-disabled-bg: var(--primary-color);
-      --bs-btn-disabled-border-color: var(--primary-color);
-      --bs-btn-color: #fff;
-      --bs-btn-hover-color: #fff;
-      --bs-btn-hover-bg: var(--primary-color);
-      --bs-btn-hover-border-color: var(--primary-color);
-      --bs-btn-focus-shadow-rgb: 49,132,253;
-      --bs-btn-active-color: #fff;
-      --bs-btn-active-bg: var(--primary-color);
-      --bs-btn-active-border-color: var(--primary-color);
-      --bs-btn-active-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
-      --bs-btn-disabled-color: #fff;
-    }
-
-    .btn:hover {
-      color: var(--bs-btn-hover-color);
-      background-color: var(--bs-btn-hover-bg);
-      border-color: var(--bs-btn-hover-border-color);
-    }
-
-    .form-check-input:checked {
-      background-color: var(--primary-color);
-      border-color: var(--primary-color);
-    }
-
-    .nav-link, .nav-link:hover, a {
-        color: var(--primary-color);
+        --bs-primary-rgb: 128, 0, 45;
     }
 
     .nav-link:hover, a:hover {
         color: #5e0323;
-    }
-
-
-    .icon-link {
-        text-decoration-color: var(--primary-color);
-    }
-
-    .list-group-item.active {
-        border-color: var(--primary-color);
-        background-color: var(--primary-color);
     }
 
     #footer {
