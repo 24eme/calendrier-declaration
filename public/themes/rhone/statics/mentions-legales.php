86488bd4d67c9a96d5298989a77d623af34b56db
JB LM <lemetayer.jb@gmail.com>
2024-09-24 09:50:53 +0200

    mentions legales pour rhone

---
+++ public/themes/rhone/statics/mentions-legales.php
@@ -0,0 +1,21 @@
+<div class="text-center">
+    <h2 class="border-bottom pb-3">Mentions Légales</h2>
+
+    <h3 class="my-3">Éditeur</h3>
+
+    <p>Ce site web est édité par :</p>
+    <p>Syndicat des vignerons des Côtes du Rhône<br />Maison des vins 6 rue des Trois Faucons - 84918 Avignon</p>
+    <p>Tél. : 33 (0)4 90 27 24 24<br />Fax : 33 (0)4 90 85 26 83</p>
+
+    <h3 class="my-3">Crédits</h3>
+
+    <p>Rédaction, design et réalisation :</p>
+    <p>24ème<br />2 Place Sainte-Opportune - 75001 Paris</p>
+    <p>Tél. : 33 (0)1 84 25 94 01<br />Email : contact@24eme.fr</p>
+
+    <h3 class="my-3">Informations légales</h3>
+
+    <p>Conformément aux dispositions de la loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous disposez d’un droit d’accès, de modification, de rectification et de suppression des données qui vous concernent.<br />Pour demander une modification, rectification ou suppression des données vous concernant, il vous suffit d’envoyer un courrier par voie postale au Syndicat des Côtes du Rhône en justifiant de votre identité.</p>
+    <p>Le service est un logiciel libre diffusé sous licence AGPL. Le projet est disponible depuis <a href="https://github.com/24eme/calendrier-declaration">github.com/24eme/calendrier-declaration</a></p>
+    <p>Le service est hébergé par la société 24eme pour le compte du Syndicat</p>
+</div>
