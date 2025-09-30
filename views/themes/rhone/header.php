f712ccaf97efa6d83104417be8a83df06e0c0647
Gabriel Poma <gpoma@24eme.fr>
2025-09-30 12:30:40 +0200

    rhone: commit dans l'image pour forcer le changement

--- views/themes/rhone/header.php
+++ views/themes/rhone/header.php
@@ -1,6 +1,6 @@
 <div class="mt-3 mb-3">
 <a href="/" class="text-start">
-<img class="img-fluid" src="/images/logos/logo_odgrhone.png" alt="logo syndicat du rhone" style="border-bottom: 1px solid #e30613;"/>
+<img class="img-fluid" src="/images/logos/logo_odgrhone.png?<?php echo getCommit() ?>" alt="logo syndicat du rhone" style="border-bottom: 1px solid #e30613;"/>
 </a>
 </div>
 <h1 class="text-uppercase text-center h5">Déclarations viti/vinicoles</h1>
